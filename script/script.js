fbb7fed801548a243c217ff86baee36484d269b6
VLadFurmanyuk <furmanyukvlad13@gmail.com>
2022-09-02 14:41:22 +0300

    Fixed #4

--- script/script.js
+++ script/script.js
@@ -10,19 +10,19 @@ $(document).ready(function (){
 });
 
 
-//Добавляем всем элементам прозрачность
-$('.slider__item').addClass('slick-slider-opacity');
-//Убираем прозрачность у центральных элементов
-$('.slider__item.slick-active').eq(1).removeClass('slick-slider-opacity');
-$('.slider__item.slick-active').eq(2).removeClass('slick-slider-opacity');
-//Вешаем обработчик на событие слайдера
-$(".slider").on("afterChange", function() {
-    //Добавляем прозрачность всем элементам, обнуляя её у нужных
-    $('.slider__item').addClass('slick-slider-opacity');
-    $('.slider__item.slick-active').eq(1).removeClass('slick-slider-opacity');
-    $('.slider__item.slick-active').eq(2).removeClass('slick-slider-opacity');
-    $('.slider__item.slick-active').eq(3).removeClass('slick-slider-opacity');
-    $('.slider__item.slick-active').eq(4).removeClass('slick-slider-opacity');
-    $('.slider__item.slick-active').eq(5).removeClass('slick-slider-opacity');
-    $('.slider__item.slick-active').eq(6).removeClass('slick-slider-opacity');
-});
+// //Добавляем всем элементам прозрачность
+// $('.slider__item').addClass('slick-slider-opacity');
+// //Убираем прозрачность у центральных элементов
+// $('.slider__item.slick-active').eq(1).removeClass('slick-slider-opacity');
+// $('.slider__item.slick-active').eq(2).removeClass('slick-slider-opacity');
+// //Вешаем обработчик на событие слайдера
+// $(".slider").on("afterChange", function() {
+//     //Добавляем прозрачность всем элементам, обнуляя её у нужных
+//     $('.slider__item').addClass('slick-slider-opacity');
+//     $('.slider__item.slick-active').eq(1).removeClass('slick-slider-opacity');
+//     $('.slider__item.slick-active').eq(2).removeClass('slick-slider-opacity');
+//     $('.slider__item.slick-active').eq(3).removeClass('slick-slider-opacity');
+//     $('.slider__item.slick-active').eq(4).removeClass('slick-slider-opacity');
+//     $('.slider__item.slick-active').eq(5).removeClass('slick-slider-opacity');
+//     $('.slider__item.slick-active').eq(6).removeClass('slick-slider-opacity');
+// });
